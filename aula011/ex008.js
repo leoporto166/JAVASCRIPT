22766d739516301606e36d5bdec6c1cb1d81c6a4
leoporto166 <leonardo.porto166@gmail.com>
2025-02-22 12:03:28 -0300

    aula 11

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,1 +1,11 @@
-console.log('O console funcionou corretamente')
+var vel = 51
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 50){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+} else{
+    console.log(`Você menteve a velocidade permitida`)
+}
+
+
+
+console.log(`Dirija sempre usando cinto de segurança`)
